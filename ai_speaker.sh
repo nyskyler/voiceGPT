e156a555559f7955501e960053f05258c2903029
유승우 <yuseung-u@yuseung-uui-Macmini.local>
2024-09-10 06:20:10 +0900

    add_option_file

---
+++ ai_speaker.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+export FLASK_APP=voiceGPT
+export FLASK_ENV=development
+export FLASK_DEBUG=true
+export FLASK_RUN_PORT=8080
+source /Users/yuseung-u/Desktop/pythonLab/voiceGPT/bin/activate 
+cd /Users/yuseung-u/Desktop/pythonLab/voiceGPT
+
